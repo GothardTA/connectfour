c0f101d382109017fa8d276b1235f2a5406b303b
GothardTA <gothardta@s.dcsdk12.org>
2024-04-23 11:34:55 -0600

    aded non working win logic

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     // etc
 
     loop {
-        display_board(board);
+        display_board(&board);
 
         let mut col: usize = 0;
         let mut lowest_row: usize = 0;
@@ -53,6 +53,16 @@ fn main() {
 
         lowest_row -= 1;
 
+        let winner = check_win(&board);
+
+        if winner == 'R' {
+            println!("Player 1 won");
+            break;
+        } else if winner == 'Y' {
+            println!("Player 2 won");
+            break;
+        }
+
         if player_turn == 1 {
             board[lowest_row][col] = 82;
             player_turn = 2;
@@ -63,6 +73,7 @@ fn main() {
     }
 }
 
+// returns input from the terminal
 fn get_input() -> String {
     let mut s = String::new();
     let _ = stdout().flush();
@@ -77,11 +88,12 @@ fn get_input() -> String {
     return s;
 }
 
-fn display_board(board: [[u8; 7]; 6]) {
+// outputs the board to the screen
+fn display_board(board: &[[u8; 7]; 6]) {
     print!("{}[2J", 27 as char);
     for row in board {
         for spot in row {
-            print!("| {} ", spot as char);
+            print!("| {} ", *spot as char);
         }
         println!("|");
         for i in 0..row.len() {
@@ -89,4 +101,39 @@ fn display_board(board: [[u8; 7]; 6]) {
         }
         println!("|");
     }
+}
+
+// checks the board for any four in a row and return the color (R, Y) that won
+fn check_win(board: &[[u8; 7]; 6]) -> char {
+    for row in 0..(board.len()-3) {
+        for col in 0..(board[row].len()-3) {
+            // vertical four in a row
+            if
+                board[row][col] == board[row+1][col] &&
+                board[row+1][col] == board[row+2][col] &&
+                board[row+2][col] == board[row+3][col]
+            {
+                return board[row][col] as char;
+            }
+
+            // horizontal four in a row
+            if
+                board[row][col] == board[row][col+1] &&
+                board[row][col+1] == board[row][col+2] &&
+                board[row][col+2] == board[row][col+3]
+            {
+                return board[row][col] as char;
+            }
+
+            // diagonal four in a row
+            if
+                board[row][col] == board[row+1][col+1] &&
+                board[row+1][col+1] == board[row+2][col+2] &&
+                board[row+2][col+2] == board[row+3][col+3]
+            {
+                return board[row][col] as char;
+            }
+        }
+    }
+    return ' ';
 }
